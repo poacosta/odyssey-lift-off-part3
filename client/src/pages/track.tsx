e64fcf3f38c001a3c501ac92cf351dd1eec1c256
Pedro Orlando Acosta Pereira <poacosta87@gmail.com>
2023-11-05 01:51:42 -0600

    8. THE USEQUERY HOOK - WITH VARIABLES

--- client/src/pages/track.tsx
+++ client/src/pages/track.tsx
@@ -3,34 +3,44 @@ import { gql } from "../__generated__";
 import { useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 import { useParams } from "react-router-dom";
+import TrackDetail from "../components/track-detail";
+
+const GET_TRACK = gql(`
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`);
 
 const Track = () => {
     const { trackId = "" } = useParams();
-    return <Layout></Layout>;
+    const { loading, error, data } = useQuery(GET_TRACK, {
+        variables: { trackId },
+    });
+    return (
+        <Layout>
+            <QueryResult error={error} loading={loading} data={data}>
+                <TrackDetail track={data?.track}/>
+            </QueryResult>
+        </Layout>
+    );
 };
-
-export const GET_TRACK = gql(`
-  query GetTrack($trackId: ID!) {
-      track(id: $trackId) {
-        id
-        title
-        author {
-          id
-          name
-          photo
-        }
-        thumbnail
-        length
-        modulesCount
-        numberOfViews
-        modules {
-          id
-          title
-          length
-        }
-        description
-      }
-  }
-)`);
 
 export default Track;
