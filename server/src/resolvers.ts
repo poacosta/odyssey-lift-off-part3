2bb6798ba718096b6617a6507f9c0a18f5397109
Pedro Orlando Acosta Pereira <poacosta87@gmail.com>
2023-11-05 01:17:15 -0600

    5. RESOLVER CHAINS

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -7,9 +7,16 @@ export const resolvers: Resolvers = {
             return dataSources.trackAPI.getTracksForHome();
         },
         // get a single track by ID, for the track page
-        track: (_, { id }, { dataSources }) => {
-            return dataSources.trackAPI.getTrack(id);
-        },
+        track: async (_, { id }, { dataSources }) => {
+            // get track details
+            const track = dataSources.trackAPI.getTrack(id);
+
+            // get module details for the track
+            const modules = await dataSources.trackAPI.getTrackModules(id);
+
+            // shape the data in the way that the schema expects it
+            return { ...track, modules };
+        }
     },
     Track: {
         author: ({ authorId }, _, { dataSources }) => {
