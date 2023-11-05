d74817ed671fb2516a7ae59212424dc97c59b192
Pedro Orlando Acosta Pereira <poacosta87@gmail.com>
2023-11-05 01:06:55 -0600

    4. RESOLVER ARGS PARAMETER

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -2,13 +2,17 @@ import { RESTDataSource } from "@apollo/datasource-rest";
 import { TrackModel, AuthorModel } from "../models"
 
 export class TrackAPI extends RESTDataSource {
-  baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+    baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
 
-  getTracksForHome() {
-    return this.get<TrackModel[]>("tracks");
-  }
+    getTracksForHome() {
+        return this.get<TrackModel[]>("tracks");
+    }
 
-  getAuthor(authorId: string) {
-    return this.get<AuthorModel>(`author/${authorId}`);
-  }
+    getAuthor(authorId: string) {
+        return this.get<AuthorModel>(`author/${authorId}`);
+    }
+
+    getTrack(trackId: string) {
+        return this.get<TrackModel>(`track/${trackId}`);
+    }
 }
